959571b707def2418ad38e509d1048fa64a060e9
ssk53 <ssk53@student.le.ac.uk>
2026-08-03 13:28:52 +0100

    Resolve navigation merge conflict

--- mobile/src/types/navigation.ts
+++ mobile/src/types/navigation.ts
@@ -79,15 +79,12 @@ export type RootStackParamList = {
   AdminAuditLogDetail: { auditLogId: string };
   AdminRegisterWebView: { title: string; url: string; helperText?: string };
   PatientProfile: { user?: any } | undefined;
-<<<<<<< HEAD
-=======
   EditPatientProfile: undefined;
   NotificationPreferences: undefined;
   ReminderSettings: undefined;
-SafetyResponseSettings: undefined;
-LanguageAccessibility: undefined;
-PrivacySecurity: undefined;
->>>>>>> multilingual-settings-integration
+  SafetyResponseSettings: undefined;
+  LanguageAccessibility: undefined;
+  PrivacySecurity: undefined;
   SelectDoctor: undefined;
   PatientActiveCalls: undefined;
   MedicineUpdates: undefined;
